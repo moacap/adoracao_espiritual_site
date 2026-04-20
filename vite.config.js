04ef65780b06c7e4a463bc021bf11fbc78fc7bcb
Moacir Pereira <moacir@example.com>
2026-04-20 08:06:12 -0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { resolve } from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: './',
+  base: '/newsite/',
   server: {
     host: true,
     port: 5173,
